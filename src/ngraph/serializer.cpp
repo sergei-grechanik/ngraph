68e1bb260a7d9a58243357d89dd0fb3ce310f723
Robert Kimball <robert.kimball@intel.com>
2019-12-31 10:19:42 -0800

    cleanup

--- src/ngraph/serializer.cpp
+++ src/ngraph/serializer.cpp
@@ -114,7 +114,6 @@ public:
     json serialize_output(const Output<Node>& output);
     json serialize_parameter_vector(const ParameterVector& parameters);
     json serialize_output_vector(const OutputVector& output_vector);
-    json serialize_node_reference(const Node& node);
     json serialize_node(const Node& node);
     json serialize_axis_set(const AxisSet& axis_set);
     json serialize_tensor_iterator_input_description(
@@ -442,7 +441,7 @@ json JSONSerializer::serialize_parameter_vector(const ParameterVector& parameter
     json json_parameters = json::array();
     for (auto param : parameters)
     {
-        json_parameters.push_back(serialize_node_reference(*param));
+        json_parameters.push_back(param->get_name());
     }
     return json_parameters;
 }
@@ -3007,16 +3006,11 @@ shared_ptr<Node> JSONDeserializer::deserialize_node(json node_js)
     return node;
 }
 
-json JSONSerializer::serialize_node_reference(const Node& n)
-{
-    return n.get_name();
-}
-
 json JSONSerializer::serialize_output(const Output<Node>& output)
 {
     json result;
     auto index = output.get_index();
-    json json_node_reference = serialize_node_reference(*output.get_node());
+    json json_node_reference = output.get_node()->get_name();
     if (index == 0)
     {
         result = json_node_reference;
@@ -3041,7 +3035,6 @@ json JSONSerializer::serialize_output_vector(const OutputVector& output_vector)
 
 json JSONSerializer::serialize_node(const Node& n)
 {
-    // m_nodes_serialized.insert(&n);
     const NodeTypeInfo& type_info = n.get_type_info();
     json jtype_info;
     jtype_info["name"] = type_info.name;
@@ -3068,7 +3061,7 @@ json JSONSerializer::serialize_node(const Node& n)
     }
     for (auto cdep : n.get_control_dependencies())
     {
-        control_deps.push_back(serialize_node_reference(*cdep));
+        control_deps.push_back(cdep->get_name());
     }
     for (auto& output : n.outputs())
     {
